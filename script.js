fe58064044772a43b69005c041514bf10db5dbff
Selena Singleton <ssingle7@gmail.com>
2020-12-16 22:21:31 -0500

    made corrections and added div classes to index.html, added variables to script.js, and added documentation to readme

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-$(document).ready(function(){
-    var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+// $(document).ready(function(){
     
-    for (var i = 0; i < arrayOfHours.length; i++){
+//sets Today's date using moment.js application 
+var todaysDate = $("#currentDay");
+todaysDate.text(moment().format( "dddd, MMMM, Do YYYY")); 
 
-        var timeBlock = $();
-    }
-});
+//create time blocks for each hour 9-5pm and style there 
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15","16", "17"];
+
+//create an element 
+var row = $("div");
+row.addClass("row time-block"); 
+
+var hour = $("div");
+hour.addClass("hour col-1");
+row.append(hour);
+
+var textArea = $("textarea");
+textArea.addClass("past description col-10");
+row.append(textArea);
+
+var button = $("button");
+button.zddClass("saveBtn col-1");
+row.append(button);
+
+$(".container").append(row);
+    
+
+// var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+    
+//     for (var i = 0; i < arrayOfHours.length; i++){
+
+//         var timeBlock = $("row");
+//     }
+// });
