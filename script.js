4ac11747206f5d80b114725dbffb2fd4784debb7
Selena Singleton <ssingle7@gmail.com>
2020-12-16 21:32:07 -0500

    added div and buttons to index.html and for loop to script.js to loop over array of hours

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
-    var arrayOfHours = 
-})
-var arrayOfHours = 
+    var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+    
+    for (var i = 0; i < arrayOfHours.length; i++){
+
+        var timeBlock = $();
+    }
+});
