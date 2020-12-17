80b9a33fa3526fd681618010c7f9967825fe57dd
Selena Singleton <ssingle7@gmail.com>
2020-12-17 17:33:42 -0500

    modified files

--- script.js
+++ script.js
@@ -7,25 +7,26 @@ todaysDate.text(moment().format( "dddd, MMMM, Do YYYY"));
 //create time blocks for each hour 9-5pm and style there 
 var timeOfDay = ["9", "10", "11", "12", "13", "14", "15","16", "17"];
 
-var currentTime = moment().format("HHmm");
+var currentTime = moment().format("Hmm");
 console.log(currentTime);
 if(parseInt(timeOfDay[0]) < parseInt(currentTime)){
     console.log("This time is in the past");
+    else()
 }
 //create an element 
-var row = $("div");
+var row = $("<div>");
 row.addClass("row time-block"); 
 
-var hour = $("div");
+var hour = $("<div>");
 hour.addClass("hour col-1");
 row.append(hour);
 
-var textArea = $("textarea");
+var textArea = $("<textarea>");
 textArea.addClass("description col-lg-8");
 row.append(textArea);
 
-var button = $("button");
-button.addClass("saveBtn col-1");
+var button = $("<button>");
+// button.addClass("saveBtn col-1 far fa-save");
 row.append(button);
 
 $(".container").append(row);
