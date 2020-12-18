1490c0a098284bd1d11d38fa65c49da8d6f050de
Selena Singleton <ssingle7@gmail.com>
2020-12-17 20:01:35 -0500

    corrected script.js file

--- script.js
+++ script.js
@@ -1,42 +1,34 @@
 // $(document).ready(function(){
- console.log("Hello World");   
-//sets Today's date using moment.js application 
+console.log("Hello World");
+//sets Today's date using moment.js application
 var todaysDate = $("#currentDay");
-todaysDate.text(moment().format( "dddd, MMMM, Do YYYY")); 
+todaysDate.text(moment().format("dddd, MMMM, Do YYYY"));
 
-//create time blocks for each hour 9-5pm and style there 
-var timeOfDay = ["9", "10", "11", "12", "13", "14", "15","16", "17"];
+//create time blocks for each hour 9-5pm and style there
+var timeOfDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-var currentTime = moment().format("Hmm");
-console.log(currentTime);
-if(parseInt(timeOfDay[0]) < parseInt(currentTime)){
-    console.log("This time is in the past");
-    else(timeOfDay[0]) !=== parseInt(currentTime)
-    console.log("This time is in the future");
+var currentTime = moment().format("H");
+
+//create an element
+for (var i=0; i < timeOfDay.length; i++){
+    var row = $("<div>");
+    row.addClass("row time-block");
+    
+    var hour = $("<div>");
+    hour.addClass("hour col-1");
+    row.append(hour);
+    var eventHour = moment().set("hour", parseInt(timeOfDay[i])).format("hA");
+    hour.text(eventHour);
+    
+    var textArea = $("<textarea>");
+    textArea.addClass("past description col-lg-8");
+    row.append(textArea);
+    
+    var button = $("<button>");
+    button.addClass("saveBtn col-1 fas fa-save");
+    row.append(button);
+    
+    $(".container").append(row);
 }
-//create an element 
-var row = $("<div>");
-row.addClass("row time-block"); 
 
-var hour = $("<div>");
-hour.addClass("hour col-1");
-row.append(hour);
 
-var textArea = $("<textarea>");
-textArea.addClass("description col-lg-8");
-row.append(textArea);
-
-var button = $("<button>");
-// button.addClass("saveBtn col-1 far fa-save");
-row.append(button);
-
-$(".container").append(row);
-    
-
-// var arrayOfHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
-    
-//     for (var i = 0; i < arrayOfHours.length; i++){
-
-//         var timeBlock = $("row");
-//     }
-// });
